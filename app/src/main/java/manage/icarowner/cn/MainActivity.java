ec5cc4d792018c0b5c50d207f781fd11d6c34ac2
ping.jiang <ping.jiang@icarowner.cn>
2017-03-31 15:18:55 +0800

    fisrt commit
    Signed-off-by: ping.jiang <ping.jiang@icarowner.cn>

---
+++ app/src/main/java/manage/icarowner/cn/MainActivity.java
@@ -0,0 +1,22 @@
+package manage.icarowner.cn;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+    }
+
+}
